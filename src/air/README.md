c48fb3513e9a964467a303e811c4eaa335cc8a66
Simon Emms <simon@simonemms.com>
2026-03-23 15:02:11 +0000

    feat: create air feature
    Signed-off-by: Simon Emms <simon@simonemms.com>

---
+++ src/air/README.md
@@ -0,0 +1,26 @@
+# Air (air)
+
+Install the Air CLI
+
+## Example Usage
+
+```json
+"features": {
+    "ghcr.io/mrsimonemms/devcontainers/air:1": {}
+}
+```
+
+## Options
+
+| Options Id | Description | Type | Default Value |
+|-----|-----|-----|-----|
+| version | Select the version you would like to install | string | latest |
+
+## Air
+
+This Feature installs [the Air CLI](https://github.com/air-verse/air)
+
+
+---
+
+_Note: This file was auto-generated from the [devcontainer-feature.json](https://github.com/mrsimonemms/devcontainers/blob/main/src/cobra-cli/devcontainer-feature.json).  Add additional notes to a `NOTES.md`._
---
+++ src/air/install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Copyright 2023 Simon Emms <simon@simonemms.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+set -e
+
+. ./library_scripts.sh
+
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-extra/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations,
+# and if missing - will download a temporary copy that automatically get deleted at the end
+# of the script
+ensure_nanolayer nanolayer_location "v0.5.6"
+
+$nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-extra/features/gh-release:1.0.25" \
+    --option repo='air-verse/air' --option binaryNames='air' --option assetRegex='\.tar\.gz$' --option version="$VERSION"
+
+echo 'Done!'
